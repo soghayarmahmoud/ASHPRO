9e9d678863d92274c698490167414d3ab2c4e7f2
mahmoudsruby <mahmoudsruby@gmail.com>
2026-04-28 03:21:40 +0300

    Change the App Design Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace ASHPRO
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            menuStrip1 = new MenuStrip();
+            filesToolStripMenuItem = new ToolStripMenuItem();
+            form1ToolStripMenuItem = new ToolStripMenuItem();
+            form2ToolStripMenuItem = new ToolStripMenuItem();
+            form3ToolStripMenuItem = new ToolStripMenuItem();
+            form4ToolStripMenuItem = new ToolStripMenuItem();
+            viewToolStripMenuItem = new ToolStripMenuItem();
+            fullScreenToolStripMenuItem = new ToolStripMenuItem();
+            exitFullScreenToolStripMenuItem = new ToolStripMenuItem();
+            closeAppToolStripMenuItem = new ToolStripMenuItem();
+            guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            guna2Button4 = new Guna.UI2.WinForms.Guna2Button();
+            guna2Button3 = new Guna.UI2.WinForms.Guna2Button();
+            guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
+            guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            menuStrip1.SuspendLayout();
+            guna2Panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(24, 24);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { filesToolStripMenuItem, viewToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(1259, 33);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // filesToolStripMenuItem
+            // 
+            filesToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { form1ToolStripMenuItem, form2ToolStripMenuItem, form3ToolStripMenuItem, form4ToolStripMenuItem });
+            filesToolStripMenuItem.Name = "filesToolStripMenuItem";
+            filesToolStripMenuItem.Size = new Size(62, 29);
+            filesToolStripMenuItem.Text = "Files";
+            // 
+            // form1ToolStripMenuItem
+            // 
+            form1ToolStripMenuItem.Name = "form1ToolStripMenuItem";
+            form1ToolStripMenuItem.Size = new Size(270, 34);
+            form1ToolStripMenuItem.Text = "Form1";
+            form1ToolStripMenuItem.Click += form1ToolStripMenuItem_Click;
+            // 
+            // form2ToolStripMenuItem
+            // 
+            form2ToolStripMenuItem.Name = "form2ToolStripMenuItem";
+            form2ToolStripMenuItem.Size = new Size(270, 34);
+            form2ToolStripMenuItem.Text = "Form2";
+            form2ToolStripMenuItem.Click += form2ToolStripMenuItem_Click;
+            // 
+            // form3ToolStripMenuItem
+            // 
+            form3ToolStripMenuItem.Name = "form3ToolStripMenuItem";
+            form3ToolStripMenuItem.Size = new Size(270, 34);
+            form3ToolStripMenuItem.Text = "Form3";
+            form3ToolStripMenuItem.Click += form3ToolStripMenuItem_Click;
+            // 
+            // form4ToolStripMenuItem
+            // 
+            form4ToolStripMenuItem.Name = "form4ToolStripMenuItem";
+            form4ToolStripMenuItem.Size = new Size(270, 34);
+            form4ToolStripMenuItem.Text = "Form4";
+            form4ToolStripMenuItem.Click += form4ToolStripMenuItem_Click;
+            // 
+            // viewToolStripMenuItem
+            // 
+            viewToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { fullScreenToolStripMenuItem, exitFullScreenToolStripMenuItem, closeAppToolStripMenuItem });
+            viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            viewToolStripMenuItem.Size = new Size(65, 29);
+            viewToolStripMenuItem.Text = "View";
+            // 
+            // fullScreenToolStripMenuItem
+            // 
+            fullScreenToolStripMenuItem.Name = "fullScreenToolStripMenuItem";
+            fullScreenToolStripMenuItem.Size = new Size(270, 34);
+            fullScreenToolStripMenuItem.Text = "Full Screen";
+            fullScreenToolStripMenuItem.Click += fullScreenToolStripMenuItem_Click;
+            // 
+            // exitFullScreenToolStripMenuItem
+            // 
+            exitFullScreenToolStripMenuItem.Name = "exitFullScreenToolStripMenuItem";
+            exitFullScreenToolStripMenuItem.Size = new Size(270, 34);
+            exitFullScreenToolStripMenuItem.Text = "Exit Full Screen";
+            exitFullScreenToolStripMenuItem.Click += exitFullScreenToolStripMenuItem_Click;
+            // 
+            // closeAppToolStripMenuItem
+            // 
+            closeAppToolStripMenuItem.Name = "closeAppToolStripMenuItem";
+            closeAppToolStripMenuItem.Size = new Size(270, 34);
+            closeAppToolStripMenuItem.Text = "Close App";
+            closeAppToolStripMenuItem.Click += closeAppToolStripMenuItem_Click;
+            // 
+            // guna2Panel1
+            // 
+            guna2Panel1.BackColor = Color.Transparent;
+            guna2Panel1.BorderRadius = 50;
+            guna2Panel1.Controls.Add(guna2Button4);
+            guna2Panel1.Controls.Add(guna2Button3);
+            guna2Panel1.Controls.Add(guna2Button2);
+            guna2Panel1.Controls.Add(guna2Button1);
+            customizableEdges9.BottomLeft = false;
+            customizableEdges9.TopLeft = false;
+            guna2Panel1.CustomizableEdges = customizableEdges9;
+            guna2Panel1.Location = new Point(959, 33);
+            guna2Panel1.Name = "guna2Panel1";
+            guna2Panel1.ShadowDecoration.CustomizableEdges = customizableEdges10;
+            guna2Panel1.Size = new Size(300, 601);
+            guna2Panel1.TabIndex = 2;
+            // 
+            // guna2Button4
+            // 
+            guna2Button4.BackColor = Color.Transparent;
+            guna2Button4.BorderRadius = 18;
+            guna2Button4.CustomizableEdges = customizableEdges1;
+            guna2Button4.DisabledState.BorderColor = Color.DarkGray;
+            guna2Button4.DisabledState.CustomBorderColor = Color.DarkGray;
+            guna2Button4.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            guna2Button4.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            guna2Button4.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            guna2Button4.ForeColor = Color.White;
+            guna2Button4.Location = new Point(30, 424);
+            guna2Button4.Name = "guna2Button4";
+            guna2Button4.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            guna2Button4.Size = new Size(270, 68);
+            guna2Button4.TabIndex = 3;
+            guna2Button4.Text = "Form 4";
+            guna2Button4.Click += guna2Button4_Click;
+            // 
+            // guna2Button3
+            // 
+            guna2Button3.BackColor = Color.Transparent;
+            guna2Button3.BorderRadius = 18;
+            guna2Button3.CustomizableEdges = customizableEdges3;
+            guna2Button3.DisabledState.BorderColor = Color.DarkGray;
+            guna2Button3.DisabledState.CustomBorderColor = Color.DarkGray;
+            guna2Button3.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            guna2Button3.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            guna2Button3.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            guna2Button3.ForeColor = Color.White;
+            guna2Button3.Location = new Point(30, 321);
+            guna2Button3.Name = "guna2Button3";
+            guna2Button3.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            guna2Button3.Size = new Size(270, 68);
+            guna2Button3.TabIndex = 2;
+            guna2Button3.Text = "Form 3";
+            guna2Button3.Click += guna2Button3_Click;
+            // 
+            // guna2Button2
+            // 
+            guna2Button2.BackColor = Color.Transparent;
+            guna2Button2.BorderRadius = 18;
+            guna2Button2.CustomizableEdges = customizableEdges5;
+            guna2Button2.DisabledState.BorderColor = Color.DarkGray;
+            guna2Button2.DisabledState.CustomBorderColor = Color.DarkGray;
+            guna2Button2.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            guna2Button2.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            guna2Button2.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            guna2Button2.ForeColor = Color.White;
+            guna2Button2.Location = new Point(30, 222);
+            guna2Button2.Name = "guna2Button2";
+            guna2Button2.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            guna2Button2.Size = new Size(270, 68);
+            guna2Button2.TabIndex = 1;
+            guna2Button2.Text = "Form 2";
+            guna2Button2.Click += guna2Button2_Click;
+            // 
+            // guna2Button1
+            // 
+            guna2Button1.BackColor = Color.Transparent;
+            guna2Button1.BorderRadius = 18;
+            guna2Button1.CustomizableEdges = customizableEdges7;
+            guna2Button1.DisabledState.BorderColor = Color.DarkGray;
+            guna2Button1.DisabledState.CustomBorderColor = Color.DarkGray;
+            guna2Button1.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            guna2Button1.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            guna2Button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            guna2Button1.ForeColor = Color.White;
+            guna2Button1.Location = new Point(30, 104);
+            guna2Button1.Name = "guna2Button1";
+            guna2Button1.ShadowDecoration.CustomizableEdges = customizableEdges8;
+            guna2Button1.Size = new Size(270, 68);
+            guna2Button1.TabIndex = 0;
+            guna2Button1.Text = "Form 1";
+            guna2Button1.Click += guna2Button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(1259, 624);
+            Controls.Add(guna2Panel1);
+            Controls.Add(menuStrip1);
+            ImeMode = ImeMode.On;
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            guna2Panel1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem filesToolStripMenuItem;
+        private ToolStripMenuItem form1ToolStripMenuItem;
+        private ToolStripMenuItem form2ToolStripMenuItem;
+        private ToolStripMenuItem form3ToolStripMenuItem;
+        private ToolStripMenuItem form4ToolStripMenuItem;
+        private ToolStripMenuItem viewToolStripMenuItem;
+        private ToolStripMenuItem fullScreenToolStripMenuItem;
+        private ToolStripMenuItem exitFullScreenToolStripMenuItem;
+        private ToolStripMenuItem closeAppToolStripMenuItem;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button4;
+        private Guna.UI2.WinForms.Guna2Button guna2Button3;
+        private Guna.UI2.WinForms.Guna2Button guna2Button2;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,150 @@
+using LibVLCSharp.Shared;
+
+
+
+namespace ASHPRO
+{
+    public partial class Form1 : Form
+    {
+
+
+
+        Form2 form2;
+        Form3 form3;
+        Form4 form4;
+        Form5 form5;
+
+        private bool isFullScreen = false;
+        private FormWindowState previousWindowState;
+        private FormBorderStyle previousBorderStyle;
+        private Rectangle previousBounds;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            // Panel Structure
+
+            guna2Panel1.Dock = DockStyle.None;
+            guna2Panel1.Width = 350;
+            guna2Panel1.Height = this.Height - 30;
+            guna2Panel1.Location = new Point(0, 35);
+            guna2Panel1.FillColor = Color.FromArgb(20, 20, 20);
+            guna2Panel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            Core.Initialize();
+            guna2Panel1.BringToFront();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            if (form2 == null || form2.IsDisposed)
+            {
+                form2 = new Form2();
+            }
+
+            form2.Show();
+        }
+
+        private void form1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (form2 == null || form2.IsDisposed)
+            {
+                form2 = new Form2();
+            }
+            form2.Show();
+        }
+
+        private void guna2Button2_Click(object sender, EventArgs e)
+        {
+            if (form3 == null || form3.IsDisposed)
+            {
+                form3 = new Form3();
+            }
+            form3.Show();
+        }
+
+        private void guna2Button3_Click(object sender, EventArgs e)
+        {
+            if (form4 == null || form4.IsDisposed)
+            {
+                form4 = new Form4();
+            }
+            form4.Show();
+        }
+
+        private void guna2Button4_Click(object sender, EventArgs e)
+        {
+            if (form5 == null || form5.IsDisposed)
+            {
+                form5 = new Form5();
+            }
+            form5.Show();
+        }
+
+        private void form2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (form3 == null || form3.IsDisposed)
+            {
+                form3 = new Form3();
+            }
+            form3.Show();
+        }
+
+        private void form3ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (form4 == null || form4.IsDisposed)
+            {
+                form4 = new Form4();
+            }
+            form4.Show();
+        }
+
+        private void form4ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (form5 == null || form5.IsDisposed)
+            {
+                form5 = new Form5();
+            }
+            form5.Show();
+        }
+
+        private void fullScreenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (isFullScreen) return;
+
+            previousWindowState = this.WindowState;
+            previousBorderStyle = this.FormBorderStyle;
+            previousBounds = this.Bounds;
+
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Normal;
+            this.Bounds = Screen.PrimaryScreen.Bounds;
+
+            isFullScreen = true;
+        }
+
+        private void exitFullScreenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!isFullScreen) return;
+
+            this.FormBorderStyle = previousBorderStyle;
+            this.WindowState = previousWindowState;
+            this.Bounds = previousBounds;
+
+            isFullScreen = false;
+        }
+
+        private void closeAppToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
+
+
